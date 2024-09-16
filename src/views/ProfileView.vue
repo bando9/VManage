d03706077a47a958c236d78d36da01b72b18aae6
bando9 <bandomega1@gmail.com>
2024-09-16 14:07:09 +0700

    menampilkan halaman profile ke route profile

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="profile">
+    <h1>name: bando</h1>
+    <h1>date of birth: 00 October 1999</h1>
+    <h1>batch: 34</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .name {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
